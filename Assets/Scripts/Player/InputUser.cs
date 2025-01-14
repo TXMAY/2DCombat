61c2da31d0eca2acfd9db578bc8bc02bd5504b29
TXMAY <juya6388king@naver.com>
2025-01-14 17:50:57 +0900

    Day 2 - Custom Unity Package

---
+++ Assets/Scripts/Player/InputUser.cs
@@ -0,0 +1,17 @@
+using CustomUtillity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputUser : SingleTon<InputUser>
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
