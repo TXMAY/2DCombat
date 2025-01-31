98a4e01578a852ca8e4530253a17558911d72372
TXMAY <juya6388king@naver.com>
2025-01-31 17:50:26 +0900

    Day 12 - Skill UI

---
+++ Assets/Scripts/UI/UIOptionView.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIOptionView : MonoBehaviour
+{
+    public void SwitchTo(GameObject _menu)
+    {
+        SwitchOff();
+
+        _menu?.SetActive(true);
+    }
+
+    public void SwitchOff()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+    }
+}
