b56eb9ea58041249988a8eef586964efe64744cc
TXMAY <juya6388king@naver.com>
2025-02-05 17:54:10 +0900

    Day 15 - Skill Tree

---
+++ Assets/Scripts/Slot/Item.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="skill",menuName ="Item/skill")]
+public class Item : ScriptableObject
+{
+    public string itemName;
+    public Sprite iconImage;
+}
